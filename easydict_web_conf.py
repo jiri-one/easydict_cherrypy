fb99853289303ca0313ed3411edff53c817af861
Němec Jiří <SC5898@engie.com>
2020-06-26 15:37:59 +0200

    multiplatform solution for file paths and some cleaning

---
+++ easydict_web_conf.py
@@ -0,0 +1,17 @@
+from settings import file_path
+import cherrypy
+
+conf = {
+    "global": {
+    "server.socket_host": "0.0.0.0"
+    },
+    "/static": {
+    "tools.staticdir.on": True,
+    "tools.staticdir.dir": file_path("static")
+    },
+    "/searchengine": {
+    "request.dispatch": cherrypy.dispatch.MethodDispatcher(),
+    "tools.response_headers.on": True,
+    "tools.response_headers.headers": [('Content-Type', 'text/html')]    
+    }
+}
