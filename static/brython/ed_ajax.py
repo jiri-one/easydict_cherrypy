b5763a706de3a01f4b896edddbcec075aad6231f
jiri.one <jiri.one@pm.me>
2020-06-28 14:15:01 +0200

    just some tweaks of front page and ajax

--- static/brython/ed_ajax.py
+++ static/brython/ed_ajax.py
@@ -1,4 +1,4 @@
-from browser import document, ajax, markdown
+from browser import document, ajax
 
 def on_complete(req):
 	if req.status == 200 or req.status == 0:
@@ -8,14 +8,11 @@ def on_complete(req):
 
 def ajax_post(button_search):
 	document["results"].html = "Loading..."
-	language = document["language"]
-	searched_text = document["searched_text"]
-	fulltext = document["fulltext"]
 	ajax.post("/searchengine",
 			  headers={"Content-Type": "application/x-www-form-urlencoded"},
-			  data={"language": language.value,
-					"searched_text": searched_text.value,
-					"fulltext": fulltext.checked
+			  data={"language": document["language"].value,
+					"searched_text": document["searched_text"].value,
+					"fulltext": document["fulltext"].checked
 					},
 			  oncomplete=on_complete)
 
@@ -23,20 +20,5 @@ def hit_enter(ev):
 	if ev.keyCode == 13:
 		ajax_post(None)
 
-def test(button):
-	#print(button.target.id)
-	#print(button.option)
-	print(document["language"].value)
-	#print(button.returnValue)
-	
 document["button_search"].bind("click", ajax_post)
 document["searched_text"].bind("keypress", hit_enter)
-document["whole_word"].checked = True
-document["language"].bind("click", test)
-
-#document["whole_word"].target.checked = True
-#fulltext = document["fulltext"]
-#fulltext.bind("click", test)
-#fulltext.checked = True
-
-#document["whole_word"].bind("click", test)
