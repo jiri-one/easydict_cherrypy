3d74a69d99ae787a5165ce9172166f66bec62194
jiri.one <nemec@jiri.one>
2020-07-02 08:56:54 +0200

    setting for uwsgi server and global settings to utf-8

--- easydict_web_conf.py
+++ easydict_web_conf.py
@@ -1,10 +1,11 @@
 from settings import file_path
 import cherrypy
 
-conf = {
+conf_desktop = {
     "global": {
-    "server.socket_host": "0.0.0.0",
-    "tools.sessions.on": True
+    "server.socket_host": "0.0.0.0", # use 127.0.0.1 if you dont need to test it on LAN
+    "tools.sessions.on": True,
+    "tools.encode.encoding": "utf-8"
     },
     "/static": {
     "tools.staticdir.on": True,
@@ -16,3 +17,20 @@ conf = {
     "tools.response_headers.headers": [('Content-Type', 'text/html')]    
     }
 }
+
+conf_uwsgi = {
+    "global": {
+    "engine.autoreload.on": False,
+    #"environment": "embedded" #for uwsgi this line is not necessary
+    },
+    "/static": {
+    "tools.staticdir.on": True,
+    "tools.staticdir.dir": file_path("static")
+    },
+    "/searchengine": {
+    "request.dispatch": cherrypy.dispatch.MethodDispatcher(),
+    "tools.response_headers.on": True,
+    "tools.response_headers.headers": [('Content-Type', 'text/html')]    
+    }
+}
+    
